b63193c7ab6afe177b4ff6e795a0b03577e15105
adityaaillur <illur.a@northeastern.edu>
2022-09-29 17:38:11 -0400

    CRD operations

---
+++ assign01/Assign_ 01/src/model/Employee.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author adityaillur
+ */
+public class Employee {
+    private String name;
+    private String empID;
+    private String age;
+    private String stDt;
+    private String lvl;
+    private String teamInfo;
+    private String posTitl;
+    private String email;
+    private String gen;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getempID() {
+        return empID;
+    }
+
+    public void setempID(String empID) {
+        this.empID = empID;
+    }
+
+    public String getage() {
+        return age;
+    }
+
+    public void setage(String age) {
+        this.age = age;
+    }
+
+    public String getstDt() {
+        return stDt;
+    }
+
+    public void setstDt(String stDt) {
+        this.stDt = stDt;
+    }
+    
+     public String getteamInfo() {
+        return teamInfo;
+    }
+
+    public void setteamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+    
+     public String getposTitl() {
+        return posTitl;
+    }
+
+    public void setposTitl(String posTitl) {
+        this.posTitl = posTitl;
+    }
+    
+     public String getemail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+    
+     public String getlvl() {
+        return lvl;
+    }
+
+    public void setlvl(String lvl) {
+        this.lvl = lvl;
+    }
+    
+     public String getgen() {
+        return gen;
+    }
+
+    public void setgen(String gen) {
+        this.gen = gen;
+    }
+}
---
+++ assign01/Assign_ 01/src/model/EmployeeHistory.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author adityaillur
+ */
+public class EmployeeHistory {
+    public ArrayList<Employee> history;
+    
+    public EmployeeHistory(){
+         this.history = new ArrayList<Employee>(); 
+        
+    }
+
+    public ArrayList<Employee> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Employee> history) {
+        this.history = history;
+    }
+    
+    public Employee addNewEmployee(){
+        
+        Employee newEmployee = new Employee();
+        history.add(newEmployee);
+        return newEmployee;
+    }
+    
+    public void deleteEmployee(Employee eh) {
+        history.remove(eh);
+    }
+    
+
+}
